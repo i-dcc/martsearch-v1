d1dc8ba1cd8feb3228ee7d0fe7938a2d1654ad74
Daz Oakley <daz.oakley@gmail.com>
2009-04-25 08:13:04 +0100

    Deleted all old gui related files - there's a lot of cruft in there that needs streamlining...

--- gui/test/unit/support.js
+++
@@ -1,9 +0,0 @@
-module("support");
-
-test("jQuery hash.keys extension", function() {
-  expect(2);
-  var hash = { "one":"foo", "two":"bar", "three":"baz" };
-  equals( $.keys(hash).length, 3, "should return 3 keys " );
-  equals( hash[$.keys(hash)[1]], "bar", "return the correct key/value ");
-});
-
--- gui/test/unit/view.js
+++
@@ -1,6 +0,0 @@
-module("view");
-
-test("first dummy test in module ", function() {
-  expect(1);
-  ok( true, "all pass " );
-});
