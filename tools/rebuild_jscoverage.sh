7ed2853b32453475c99ec4dc9d77b5bb8aa13dac
Daz Oakley <daz.oakley@gmail.com>
2009-04-28 18:37:17 +0100

    Update to jscoverage building script to accommodate directory name changes.

--- tools/rebuild_jscoverage.sh
+++ tools/rebuild_jscoverage.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-rm -rf /Users/$USER/projects/martsearch2/test_coverage &&
+rm -rf /Users/$USER/projects/martsearch/test_coverage &&
 cd /Users/$USER/projects &&
-jscoverage --no-instrument=test --no-instrument=js/utils martsearch2 test_coverage &&
-mv test_coverage martsearch2/
+jscoverage --no-instrument=test --no-instrument=js/utils martsearch test_coverage &&
+mv test_coverage martsearch/
