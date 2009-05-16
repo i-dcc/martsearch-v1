37fea02de096d6f293e5262beb6aa41c9d8ac5f0
Daz Oakley <daz.oakley@gmail.com>
2009-05-16 09:49:03 +0100

    Started to expand the test suite - the test coverage is far too low...

--- test/unit/dataset.js
+++ test/unit/dataset.js
@@ -1,6 +1,46 @@
 module("dataset");
 
-test("first dummy test in module ", function() {
-  expect(1);
-  ok( true, "all pass " );
+// Load in the dataset configs so we can check some details...
+var datasets = [];
+jQuery.ajax({
+  url:      ms.base_url + "/bin/dataset-feed.pl",
+  type:     'GET',
+  dataType: 'json',
+  async:    false,
+  success:  function (ds) {
+    datasets = ds;
+  }
 });
+
+for (var i=0; i < ms.datasets.length; i++) {
+  var ds = ms.datasets[i];
+  var ds_chk = datasets[i];
+  
+  test( ds.display_name+" - Basic object attributes ", function() {
+    expect(3);
+    equals( ds.display_name, ds_chk.display_name, "Dataset 'display_name' is correct " );
+    equals( ds.enabled_attributes instanceof Array, true, "'enabled_attributes' is an array " );
+    equals( ds.mart_dataset, ds_chk.mart_dataset, "Pointing to the correct dataset " );
+  });
+
+  test( ds.display_name+" - Retrieving all biomart attributes dynamically ", function() {
+    ds.attributes = ds.fetch_all_attributes();
+
+    expect(3);
+    ok( jQuery.keys(ds.attributes).length > 0, "We returned some attributes " );
+    equals( ds.attributes instanceof Object, true, "'attributes' is an object " );
+    
+    // Deliberatly fail the fetch...
+    var real_url = ds.url;
+    ds.url = '/foo';
+    ds.attributes = ds.fetch_all_attributes();
+    ok( jQuery.keys(ds.attributes).length == 0, "We failed to fetch any attributes " );
+
+    ds.url = real_url;
+    ds.attributes = ds.fetch_all_attributes();
+  });
+  
+  
+};
+
+
--- test/unit/index.js
+++ test/unit/index.js
@@ -1,2 +1,23 @@
 module("index");
 
+var index = ms.index;
+
+test("Ping the index ", function() {
+  expect(2);
+  
+  // First, just make sure the initially configured index is ok
+  ok( index.is_alive(), "Initially configured index is alive " );
+  
+  // Change the index url to deliberately make the ping fail...
+  var real_url = index.url;
+  index.url = '/foo';
+  equals( index.is_alive(), false, "Bad index ping returned false " );
+  index.url = real_url;
+});
+
+test("Searching with the index ", function() {
+  var results = index.search( "cbx", 0 );
+  ok( results instanceof Object, "Got a results object " );
+  ok( results.response.numFound >= 0, "numFound is returning a number " );
+  ok( results.response.docs instanceof Array, "Got the 'docs' array " );
+});
